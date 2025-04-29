10835411dd2cc6bb0d1940e8b8d6ba0903473696
Lucas Barão <lucas.baraom@gmail.com>
2025-04-29 02:29:36 -0300

    adicionando apiCLient no campeonato service

--- lib/service/campeonato_service.dart
+++ lib/service/campeonato_service.dart
@@ -1,33 +1,36 @@
 import 'package:dio/dio.dart';
+import 'package:futeba/helpers/api_client.dart';
 import 'package:futeba/helpers/crud_service.dart';
+import 'package:futeba/helpers/injector.dart';
 import 'package:futeba/models/campeonato.dart';
 
 class CampeonatoService implements CrudService<Campeonato> {
-  final Dio dio = Dio();
+  final apiClient = Injector.container<ApiClient>();
+
   @override
   Future<Campeonato> atualizar(Campeonato campeonato, Map<String, dynamic> dados, [Map<String, dynamic>? opcoesExtras]) async {
-    final resposta = await dio.put("http://localhost:8000/campeonatos/${campeonato.id}", data: dados);
+    final resposta = await apiClient.put("/campeonatos/${campeonato.id}", dados);
 
     return Campeonato.fromJson(resposta.data);
   }
 
   @override
   Future<Campeonato> buscar(int id, [Map<String, dynamic>? opcoesExtras]) async {
-    final resposta = await dio.get("http://localhost:8000/campeonatos/$id");
+    final resposta = await apiClient.get("/campeonatos/$id");
 
     return Campeonato.fromJson(resposta.data);
   }
 
   @override
   Future<Campeonato> cadastrar(Map<String, dynamic> dados, [Map<String, dynamic>? opcoesExtras]) async {
-    final resposta = await dio.post("http://localhost:8000/campeonatos", data: dados);
+    final resposta = await apiClient.post("/campeonatos", dados);
 
     return Campeonato.fromJson(resposta.data);
   }
 
   @override
   Future<List<Campeonato>> listar({Map<String, dynamic>? parametros, Map<String, dynamic>? opcoesExtras}) async {
-    final resposta = await dio.get("http://localhost:8000/campeonatos");
+    final resposta = await apiClient.get("/campeonatos");
     
     print(resposta.data);
 
@@ -36,7 +39,6 @@ class CampeonatoService implements CrudService<Campeonato> {
 
   @override
   Future remover(Campeonato campeonato, [Map<String, dynamic>? opcoesExtras]) async {
-    await dio.delete('http://localhost:8000/campeonatos/${campeonato.id}');
+    await apiClient.delete('/campeonatos/${campeonato.id}');
   }
-
 }
